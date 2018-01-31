340267884449c56593f00627ccb01da000e09d57
pingyi Luo <pingyiluo@gmail.com>
2018-01-31 20:50:35 +0800

    update

--- index.js
+++ index.js
@@ -2,17 +2,15 @@
 const koa = require('koa');
 const path = require('path');
 const serve = require('koa-static');
+const logger = require('koa-logger');
 const bodyParser = require('koa-bodyparser');
 const router = require('./src/router');
-const logger = require('./src/logger');
 const handlerError = require('./src/controller/handlerError');
 const app = new koa();
 const PORT = 3030;
 
-//静态文件服务
-app.use(serve(path.join(__dirname, 'static')));
-//日志模块
-app.use(logger);
+//koa-logger日志模块
+app.use(logger());
 //错误处理
 app.use(handlerError);
 //请求体数据处理
@@ -21,6 +19,8 @@ app.use(bodyParser({
 }));
 //路由模块
 app.use(router.routes());
+//静态文件服务
+app.use(serve(path.join(__dirname, 'static')));
 //监听请求
 app.listen(PORT, "0.0.0.0", () => {
     console.log(`server is running at 0.0.0.0:${PORT}`);
